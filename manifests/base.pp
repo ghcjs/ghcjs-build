1536768c0b9aa408ef499e413603ff5516e494db
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2013-05-19 03:12:49 +1200

    Update Gen2 build so that it includes various patched versions of hackage packages using cabal-src.

--- manifests/base.pp
+++ manifests/base.pp
@@ -40,14 +40,91 @@ export LC_ALL=en_US.utf8
 ./sync-all -r https://github.com/ghc get &&
 # ./sync-all -r https://github.com/ghcjs --ghcjs get &&
 cabal update &&
-wget http://hdiff.luite.com/tmp/0006-ghc-all.patch &&
-patch -p1 < 0006-ghc-all.patch &&
+wget http://ghcjs.github.io/patches/ghc-ghcjs.patch &&
+patch -p1 < ghc-ghcjs.patch &&
 echo 'BuildFlavour = quick' > mk/build.mk &&
 cat mk/build.mk.sample >> mk/build.mk &&
 perl boot &&
-./configure --prefix=$HOME/ghc &&
+./configure --prefix=/home/build/ghc &&
 make -j5 &&
 make install &&
+export PATH=/home/build/ghc/bin:$PATH
+cd .. &&
+
+cabal install packedstring --ghc-options='-XStandaloneDeriving -XDeriveDataTypeable' &&
+
+wget http://ghcjs.github.io/packages/cabal-src/bzlib-conduit/0.2.1.1/bzlib-conduit-0.2.1.1.tar.gz &&
+tar -xzf bzlib-conduit-0.2.1.1.tar.gz &&
+cd bzlib-conduit-0.2.1.1 &&
+cabal-src-install --src-only &&
+cd .. &&
+
+wget http://ghcjs.github.io/packages/cabal-src/vector/0.10.9/vector-0.10.9.tar.gz &&
+tar -xzf vector-0.10.9.tar.gz &&
+cd vector-0.10.9 &&
+cabal-src-install --src-only &&
+cd .. &&
+
+wget http://ghcjs.github.io/packages/cabal-src/optparse-applicative/0.5.2.1.1/optparse-applicative-0.5.2.1.1.tar.gz &&
+tar -xzf optparse-applicative-0.5.2.1.1.tar.gz &&
+cd optparse-applicative-0.5.2.1.1 &&
+cabal-src-install --src-only &&
+cd .. &&
+
+wget http://ghcjs.github.io/packages/cabal-src/MonadCatchIO-transformers/0.3.0.0.1/MonadCatchIO-transformers-0.3.0.0.1.tar.gz &&
+tar -xzf MonadCatchIO-transformers-0.3.0.0.1.tar.gz &&
+cd MonadCatchIO-transformers-0.3.0.0.1 &&
+cabal-src-install --src-only &&
+cd .. &&
+
+wget http://ghcjs.github.io/packages/cabal-src/lens/3.10.0.0.1/lens-3.10.0.0.1.tar.gz &&
+tar -xzf lens-3.10.0.0.1.tar.gz &&
+cd lens-3.10.0.0.1 &&
+cabal-src-install --src-only &&
+cd .. &&
+
+wget http://ghcjs.github.io/packages/cabal-src/aeson/0.6.1.0.1/aeson-0.6.1.0.1.tar.gz &&
+tar -xzf aeson-0.6.1.0.1.tar.gz &&
+cd aeson-0.6.1.0.1 &&
+cabal-src-install --src-only &&
+cd .. &&
+
+wget http://ghcjs.github.io/packages/cabal-src/yaml/0.8.2.3/yaml-0.8.2.3.tar.gz &&
+tar -xzf yaml-0.8.2.3.tar.gz &&
+cd yaml-0.8.2.3 &&
+cabal-src-install --src-only &&
+cd .. &&
+
+wget http://ghcjs.github.io/packages/cabal-src/jmacro/0.6.4.0.1/jmacro-0.6.4.0.1.tar.gz &&
+tar -xzf yaml-0.8.2.3.tar.gz &&
+cd yaml-0.8.2.3 &&
+cabal-src-install --src-only &&
+cd .. &&
+
+wget http://ghcjs.github.io/packages/cabal-src/haskell-src-meta/0.6.0.2.1/haskell-src-meta-0.6.0.2.1.tar.gz &&
+tar -xzf haskell-src-meta-0.6.0.2.1.tar.gz &&
+cd haskell-src-meta-0.6.0.2.1 &&
+cabal-src-install --src-only &&
+cd .. &&
+
+wget http://ghcjs.github.io/packages/cabal-src/th-orphans/0.6.0.0.1/th-orphans-0.6.0.0.1.tar.gz &&
+tar -xzf th-orphans-0.6.0.0.1.tar.gz &&
+cd th-orphans-0.6.0.0.1 &&
+cabal-src-install --src-only &&
+cd .. &&
+
+wget http://ghcjs.github.io/packages/cabal-src/th-lift/0.5.5.0.1/th-lift-0.5.5.0.1.tar.gz &&
+tar -xzf th-lift-0.5.5.0.1.tar.gz &&
+cd th-lift-0.5.5.0.1 &&
+cabal-src-install --src-only &&
+cd .. &&
+
+git clone https://github.com/ghcjs/ghcjs.git &&
+cd ghcjs &&
+git checkout unbox &&
+cabal update &&
+cabal install cabal-src &&
+
 echo Done) 2>&1| tee /tmp/build.log 
 ",
      mode => 0755,

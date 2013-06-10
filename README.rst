a0982ed1180abef1b23246e47808fc4202929561
Luite Stegeman <stegeman@gmail.com>
2013-06-10 13:40:54 +0200

    vagrant config for building the files for running the test suite

--- README.rst
+++ README.rst
@@ -2,28 +2,5 @@
 A Vagrant setup to build ghcjs
 ======
 
-Everything should build if you do a 'vagrant up', but note that it takes many
-hours because of the time to build ghcjs-ghc.
-
-When it is complete, type::
-
-  vagrant ssh
-
-You are now ready to build ghcjs programs.
-
-You can copy your outputs into /tmp/outputs and they will appear on
-the host in ghcjs-build/outputs
-
-JSShell and Node.js are included in the PATH.
-
-Some examples are built as as part of the build process and
-you can run them in your web browser using warp static.
-Port 3000 on the guest VM is redirected to port 3030 on
-the host.  You can run the examples in your web browser
-with::
-
-   vagrant ssh -c warp
-
-Then open http://localhost:3030/ in your host machines web
-browser.
+This branch builds the files required to run the test suite
 
--- manifests/base.pp
+++ manifests/base.pp
@@ -4,19 +4,15 @@ exec { "apt-update":
 }
 package { 'ghc': ensure => present }
 package { 'cabal-install': ensure => present }
-package { 'libwebkitgtk-3.0-dev': ensure => present }
 package { 'happy': ensure => present }
 package { 'alex': ensure => present }
 package { 'autoconf': ensure => present }
 package { 'libtool': ensure => present }
 package { 'darcs': ensure => present }
 package { 'git': ensure => present }
+package { 'make': ensure => present }
 package { 'libncurses5-dev': ensure => present }
 package { 'libbz2-dev': ensure => present }
-package { 'default-jre': ensure => present }
-package { 'libgl1-mesa-dev': ensure => present }
-package { 'libglu1-mesa-dev': ensure => present }
-package { 'freeglut3-dev': ensure => present }
 package { 'unzip': ensure => present }
 vcsrepo { '/home/vagrant/ghc-source':
           ensure   => latest,
@@ -24,16 +20,23 @@ vcsrepo { '/home/vagrant/ghc-source':
           owner => vagrant,
           user => vagrant,
           revision => 'master',
-          # source   => '/tmp/ghc-repo'
           source => 'https://github.com/ghc/ghc'
         }
 
-file { '/home/vagrant/ghc-source/dobuild.sh':
+file { '/home/vagrant/dobuild.sh':
      content => "#!/bin/bash
 export HOME=/home/vagrant
 export LC_ALL=en_US.utf8
 echo 'PATH=/home/vagrant/ghcjs/bin:/home/vagrant/.cabal/bin:/home/vagrant/ghc/bin:/home/vagrant/jsshell:/home/vagrant/node-v0.10.10-linux-x86/bin:\$PATH' >> /home/vagrant/.profile
 echo 'LC_ALL=en_US.utf8' >> /home/vagrant/.profile
+
+install_src_pkg() {
+   wget \"http://ghcjs.github.io/packages/cabal-src/\$1/\$2/\$1-\$2.tar.gz\"
+   tar -xzf \"\$1-\$2.tar.gz\"
+   cd \"\$1-\$2\"
+   cabal-src-install --src-only
+   cd ..
+}
 
 (cd /home/vagrant &&
 
@@ -41,128 +44,55 @@ mkdir jsshell &&
 cd jsshell &&
 wget http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/jsshell-linux-i686.zip &&
 unzip jsshell-linux-i686.zip &&
+rm jsshell-linux-i686.zip &&
 cd .. &&
 
 wget http://nodejs.org/dist/v0.10.10/node-v0.10.10-linux-x86.tar.gz &&
 tar -xzf node-v0.10.10-linux-x86.tar.gz &&
+rm node-v0.10.10-linux-x86.tar.gz &&
 
 cd ghc-source &&
-# ./sync-all -r /tmp/ghcjs-libraries get &&
-# ./sync-all -r /tmp/ghcjs-libraries --ghcjs get &&
 ./sync-all -r https://github.com/ghc get &&
 ./sync-all -r https://github.com/ghc get &&
 ./sync-all -r https://github.com/ghc get &&
 ./sync-all -r https://github.com/ghc get &&
-# ./sync-all -r https://github.com/ghcjs --ghcjs get &&
 cabal update &&
+wget http://hdiff.luite.com/ghcjs/ghc-bswap.patch &&
+patch -p1 < ghc-bswap.patch &&
 wget http://ghcjs.github.io/patches/ghc-ghcjs.patch &&
 patch -p1 < ghc-ghcjs.patch &&
 echo 'BuildFlavour = quick' > mk/build.mk &&
 cat mk/build.mk.sample >> mk/build.mk &&
-echo 'SRC_HC_OPTS     += -opta-U__i686' >> mk/build.mk &&
 perl boot &&
 ./configure --prefix=/home/vagrant/ghc &&
-
 cp -r . ../ghcjs-boot &&
-
 make -j5 &&
 make install &&
 hash -r &&
 cd .. &&
 
 (cabal update || cabal update || cabal update) &&
-cabal install packedstring --ghc-options='-XStandaloneDeriving -XDeriveDataTypeable' &&
 cabal install cabal-src &&
 
-wget http://ghcjs.github.io/packages/cabal-src/bzlib-conduit/0.2.1.1/bzlib-conduit-0.2.1.1.tar.gz &&
-tar -xzf bzlib-conduit-0.2.1.1.tar.gz &&
-cd bzlib-conduit-0.2.1.1 &&
-cabal-src-install --src-only &&
-cd .. &&
-
-wget http://ghcjs.github.io/packages/cabal-src/vector/0.10.9/vector-0.10.9.tar.gz &&
-tar -xzf vector-0.10.9.tar.gz &&
-cd vector-0.10.9 &&
-cabal-src-install --src-only &&
-cd .. &&
-
-wget http://ghcjs.github.io/packages/cabal-src/optparse-applicative/0.5.2.1.1/optparse-applicative-0.5.2.1.1.tar.gz &&
-tar -xzf optparse-applicative-0.5.2.1.1.tar.gz &&
-cd optparse-applicative-0.5.2.1.1 &&
-cabal-src-install --src-only &&
-cd .. &&
-
-wget http://ghcjs.github.io/packages/cabal-src/MonadCatchIO-transformers/0.3.0.0.1/MonadCatchIO-transformers-0.3.0.0.1.tar.gz &&
-tar -xzf MonadCatchIO-transformers-0.3.0.0.1.tar.gz &&
-cd MonadCatchIO-transformers-0.3.0.0.1 &&
-cabal-src-install --src-only &&
-cd .. &&
-
-wget http://ghcjs.github.io/packages/cabal-src/lens/3.10.0.0.1/lens-3.10.0.0.1.tar.gz &&
-tar -xzf lens-3.10.0.0.1.tar.gz &&
-cd lens-3.10.0.0.1 &&
-cabal-src-install --src-only &&
-cd .. &&
-
-wget http://ghcjs.github.io/packages/cabal-src/aeson/0.6.1.0.1/aeson-0.6.1.0.1.tar.gz &&
-tar -xzf aeson-0.6.1.0.1.tar.gz &&
-cd aeson-0.6.1.0.1 &&
-cabal-src-install --src-only &&
-cd .. &&
-
-wget http://ghcjs.github.io/packages/cabal-src/yaml/0.8.2.3/yaml-0.8.2.3.tar.gz &&
-tar -xzf yaml-0.8.2.3.tar.gz &&
-cd yaml-0.8.2.3 &&
-cabal-src-install --src-only &&
-cd .. &&
-
-wget http://ghcjs.github.io/packages/cabal-src/haskell-src-meta/0.6.0.2.1/haskell-src-meta-0.6.0.2.1.tar.gz &&
-tar -xzf haskell-src-meta-0.6.0.2.1.tar.gz &&
-cd haskell-src-meta-0.6.0.2.1 &&
-cabal-src-install --src-only &&
-cd .. &&
-
-wget http://ghcjs.github.io/packages/cabal-src/th-orphans/0.6.0.0.1/th-orphans-0.6.0.0.1.tar.gz &&
-tar -xzf th-orphans-0.6.0.0.1.tar.gz &&
-cd th-orphans-0.6.0.0.1 &&
-cabal-src-install --src-only &&
-cd .. &&
-
-wget http://ghcjs.github.io/packages/cabal-src/th-lift/0.5.5.0.1/th-lift-0.5.5.0.1.tar.gz &&
-tar -xzf th-lift-0.5.5.0.1.tar.gz &&
-cd th-lift-0.5.5.0.1 &&
-cabal-src-install --src-only &&
-cd .. &&
-
-wget http://ghcjs.github.io/packages/cabal-src/time/1.4.0.2.1/time-1.4.0.2.1.tar.gz &&
-tar -xzf time-1.4.0.2.1.tar.gz &&
-cd time-1.4.0.2.1 &&
-cabal-src-install --src-only &&
-cd .. &&
-
-wget http://ghcjs.github.io/packages/cabal-src/HTTP/4000.2.6.0.1/HTTP-4000.2.6.0.1.tar.gz &&
-tar -xzf HTTP-4000.2.6.0.1.tar.gz &&
-cd HTTP-4000.2.6.0.1 &&
-cabal-src-install --src-only &&
-cd .. &&
-
-wget http://ghcjs.github.io/packages/cabal-src/entropy/0.2.2.1/entropy-0.2.2.1.tar.gz &&
-tar -xzf entropy-0.2.2.1.tar.gz &&
-cd entropy-0.2.2.1 &&
-cabal-src-install --src-only &&
-cd .. &&
-
-wget http://ghcjs.github.io/packages/cabal-src/Tensor/1.0.0.1.1/Tensor-1.0.0.1.1.tar.gz &&
-tar -xzf Tensor-1.0.0.1.1.tar.gz &&
-cd Tensor-1.0.0.1.1 &&
-cabal-src-install --src-only &&
-cd .. &&
-
-wget http://ghcjs.github.io/packages/cabal-src/jmacro/0.6.7.0.1/jmacro-0.6.7.0.1.tar.gz &&
-tar -xzf jmacro-0.6.7.0.1.tar.gz &&
-cd jmacro-0.6.7.0.1 &&
-cabal-src-install --src-only &&
-cd .. &&
+( mkdir pkg
+  cd pkg
+install_src_pkg 'bzlib-conduit' '0.2.1.1'
+install_src_pkg 'vector' '0.10.9'
+install_src_pkg 'optparse-applicative' '0.5.2.1.1'
+install_src_pkg 'MonadCatchIO-transformers' '0.3.0.0.1'
+install_src_pkg 'contravariant' '0.4.1.1'
+install_src_pkg 'lens' '3.10.0.0.1'
+install_src_pkg 'aeson' '0.6.1.0.1'
+install_src_pkg 'yaml' '0.8.2.3'
+install_src_pkg 'haskell-src-meta' '0.6.0.2.1'
+install_src_pkg 'th-orphans' '0.6.0.0.1'
+install_src_pkg 'th-lift' '0.5.5.0.1'
+install_src_pkg 'time' '1.4.0.2.1'
+install_src_pkg 'HTTP' '4000.2.6.0.1'
+install_src_pkg 'entropy' '0.2.2.1'
+install_src_pkg 'Tensor' '1.0.0.1.1'
+install_src_pkg 'jmacro' '0.6.7.0.1'
+) &&
 
 git clone https://github.com/haskell/cabal.git &&
 cd cabal &&
@@ -174,112 +104,39 @@ cabal install &&
 cd ../cabal-install &&
 cabal install &&
 cd ../.. &&
+rm -rf cabal &&
 hash -r &&
 
 git clone https://github.com/ghcjs/ghcjs.git &&
 cd ghcjs &&
-git checkout unbox &&
-cabal install -f-compiler-only &&
+cabal install -f-compiler-only --enable-tests &&
 
 cd ../ghcjs-boot &&
 ghcjs-boot --init &&
-cd libraries/bytestring &&
-cabal install --ghcjs &&
-cd ../unix &&
-cabal install --ghcjs --constraint='bytestring>=0.10.3.0' &&
-cd ../directory &&
-cabal install --ghcjs --constraint='bytestring>=0.10.3.0' &&
-cd ../process &&
-cabal install --ghcjs --constraint='bytestring>=0.10.3.0' &&
-cd ../../.. &&
 
-cd .ghcjs/i386-linux-* &&
-git clone https://github.com/ghcjs/shims &&
-cd ../.. &&
+cd /home &&
+tar -cJvf ghcjs-test.tar.xz vagrant &&
+cp ghcjs-test.tar.gz /vagrant/ghcjs-test.tar.xz &&
 
-git clone https://github.com/ghcjs/ghcjs-prim &&
-cd ghcjs-prim &&
-cabal install --ghcjs &&
-cd .. &&
-
-git clone https://github.com/ghcjs/ghcjs-base &&
-cd ghcjs-base &&
-cabal install --ghcjs &&
-cd .. &&
-
-git clone https://github.com/ghcjs/ghcjs-jquery &&
-cd ghcjs-jquery &&
-cabal install --ghcjs &&
-cd .. &&
-
-cabal unpack regex-posix-0.95.2 &&
-cd regex-posix-0.95.2 &&
-cabal configure --constraint='bytestring>=0.10.3.0' &&
-cabal build &&
-cabal install --ghcjs --constraint='bytestring>=0.10.3.0' &&
-cd .. &&
-
-git clone https://github.com/ghcjs/ghcjs-examples.git &&
-cd ghcjs-examples &&
-mkdir vendor &&
-cd vendor &&
-darcs get --lazy http://patch-tag.com/r/hamish/gtk2hs &&
-cabal install ./gtk2hs/tools &&
-cd .. &&
-
-cabal install cabal-meta &&
-(cabal-meta install --ghcjs -fwebkit1-8 -fgtk3 --force-reinstalls --constraint='bytestring>=0.10.3.0' ||
- cabal-meta install --ghcjs -fwebkit1-8 -fgtk3 --force-reinstalls --constraint='bytestring>=0.10.3.0') &&
-cd .. &&
-
-cd &&
-cd ghcjs-examples/weblog &&
-./build.sh &&
-cd ../.. &&
-
-cabal install warp-static &&
-
-echo Done) 2>&1| tee /tmp/build.log 
+echo Done) 2>&1| tee -a /tmp/build.log
 ",
      mode => 0755,
      require => Vcsrepo['/home/vagrant/ghc-source']
      }
-# exec { 'unpackghc':
-#      command => "/bin/tar -xjf /tmp/install-archives/ghc-7.6.3-i386-unknown-linux.tar.bz2 -C /usr/src",
-#      creates => '/usr/src/ghc-7.6.3'
-#     }
-# exec { 'unpackcabal':
-#      command => "/bin/tar -xzf /tmp/install-archives/cabal-install-1.16.0.2.tar.gz -C /usr/src",
-#      creates => '/usr/src/cabal-install-1.16.0.2'
-#      }
-# exec { 'installcabal':
-#        command => 'chmod +rx bootstrap.sh && ./bootstrap.sh --global',
-#        provider => 'shell',
-#        cwd => '/usr/src/cabal-install-1.16.0.2',
-#        creates => '/usr/local/bin/cabal',
-#        require => [Exec['installghc'], Exec['unpackcabal']]
-#      }
 file { '/usr/lib/libgmp.so.3':
         ensure => link,
         target => '/usr/lib/i386-linux-gnu/libgmp.so.10'
      }
-# exec { 'installghc':
-#   command => './configure --prefix=/usr/local && make install',
-#   provider => 'shell',
-#   cwd => '/usr/src/ghc-7.6.3',
-#   creates => '/usr/local/bin/ghc',
-#   subscribe => Exec['unpackghc'],
-#   require => [Package['ghc'], Package['cabal-install'], File['/usr/lib/libgmp.so.3']]
-# }
 exec { 'build':
   provider => 'shell',
   timeout => 100000,
-  command => "/home/vagrant/ghc-source/dobuild.sh",
+  command => "/home/vagrant/dobuild.sh",
   path => "/home/vagrant/ghcjs/bin:/home/vagrant/.cabal/bin:/home/vagrant/ghc/bin:/usr/sbin:/usr/bin:/sbin:/bin",
   creates => '/home/vagrant/ghc/bin/ghc',
-  subscribe => [Vcsrepo['/home/vagrant/ghc-source'], File['/home/vagrant/ghc-source/dobuild.sh']],
+  subscribe => [Vcsrepo['/home/vagrant/ghc-source'], File['/home/vagrant/dobuild.sh']],
   user => vagrant,
   require => [Package['cabal-install'], Package['ghc'],
-              Vcsrepo['/home/vagrant/ghc-source'], File['/home/vagrant/ghc-source/dobuild.sh'], Package['happy'], Package['autoconf'],
-              Package['libtool'], Package['alex'], Package['libbz2-dev'], Package['darcs'], Package['libncurses5-dev']]
+              Vcsrepo['/home/vagrant/ghc-source'], File['/home/vagrant/dobuild.sh'], Package['happy'], Package['autoconf'],
+              Package['libtool'], Package['alex'], Package['libbz2-dev'], Package['darcs'], Package['libncurses5-dev'],
+              Package['make']]
 }

1f824ab442d1ca06efbca89830bca1d3d14d9ff6
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2013-06-29 13:24:18 +1200

    Set PATH and LC_ALL in .bashrc not .profile

--- scripts/prep0_profile.sh
+++ scripts/prep0_profile.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-echo 'PATH=/home/vagrant/ghcjs/bin:/home/vagrant/.cabal/bin:/home/vagrant/ghc/bin:/home/vagrant/jsshell:/home/vagrant/node-v0.10.10-linux-x86/bin:$PATH' >> /home/vagrant/.profile
-echo 'LC_ALL=en_US.utf8' >> /home/vagrant/.profile
+echo 'PATH=/home/vagrant/ghcjs/bin:/home/vagrant/.cabal/bin:/home/vagrant/ghc/bin:/home/vagrant/jsshell:/home/vagrant/node-v0.10.10-linux-x86/bin:$PATH' > /home/vagrant/tmpbashrc
+echo 'LC_ALL=en_US.utf8' >> /home/vagrant/tmpbashrc
+cat /home/vagrant/.bashrc >> /home/vagrant/tmpbashrc
+mv -f /home/vagrant/tmpbashrc /home/vagrant/.bashrc
 
 touch /home/vagrant/prep0 &&
 

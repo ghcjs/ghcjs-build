a7308585b0afe787a174cac8445b349e60bba354
Luite Stegeman <stegeman@gmail.com>
2013-08-03 16:06:44 +0200

    bump jmacro to 0.7.0

--- scripts/stage1_cabalsrc.sh
+++ scripts/stage1_cabalsrc.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 export HOME=/home/vagrant
 export LC_ALL=en_US.utf8
 cd /home/vagrant
@@ -35,7 +36,7 @@ install_src_pkg 'time' '1.4.0.2.1' &&
 install_src_pkg 'HTTP' '4000.2.6.0.1' &&
 install_src_pkg 'entropy' '0.2.2.2' &&
 install_src_pkg 'Tensor' '1.0.0.1.1' &&
-install_src_pkg 'jmacro' '0.6.7.0.1' &&
+install_src_pkg 'jmacro' '0.7.0' &&
 install_src_pkg 'generic-deriving' '1.5.0.0.1' &&
 install_src_pkg 'tagged' '0.6.2' &&
 install_src_pkg 'crypto-api' '0.12.2.1.1' &&

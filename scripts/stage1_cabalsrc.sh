55c79842642f0150c98eee94132349cbb7d7a224
Luite Stegeman <stegeman@gmail.com>
2013-08-14 22:13:38 +0200

    add updated primitive package

--- scripts/stage1_cabalsrc.sh
+++ scripts/stage1_cabalsrc.sh
@@ -38,6 +38,7 @@ install_src_pkg 'Tensor' '1.0.0.1.1' &&
 install_src_pkg 'jmacro' '0.7.0' &&
 install_src_pkg 'generic-deriving' '1.5.0.0.1' &&
 install_src_pkg 'crypto-api' '0.12.2.1.2' &&
+install_src_pkg 'primitive' '0.5.0.1.1'
 ) &&
 
 touch /home/vagrant/build1 &&
